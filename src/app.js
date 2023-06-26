d980be9fec9d7a8c1418b7b14b9db9ee69c458cd
sabiss <sabrinabezsilva@gmail.com>
2023-06-26 17:05:08 -0300

    pasta com as importações e rotas para o express

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import { Express } from "express-serve-static-core"; //importando o framework express
+
+const app = express();//inicializando 
+app.use(express.json);
+
+const livros = [
+    {id: 1, 'título': 'Alice no País das Maravilhas', 'autor(a)': 'Lewis Carroll'},
+    {id: 2, 'título': 'Dom Casmurro', 'autor(a)': 'Machado de Assis'}
+]
+
+app.get('/', (req, res) =>{//sempre que na url tiver '/' irá retornar este o texto 'livraria'
+    res.status(200).send("Livraria");
+})
+
+export default app;
