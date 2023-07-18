67301be765168bfd69269fe23aade413d1ffc83a
sabiss <sabrinabezsilva@gmail.com>
2023-07-17 21:05:31 -0300

    .

--- src/routes/livrosRouter.js
+++ src/routes/livrosRouter.js
@@ -2,10 +2,10 @@ import express from "express";
 import livrosController from "../controllers/livrosController.js";
 
 const router = express.Router();
-
+//se chamarem um get para o /livros faça o livros.Controller.listarLivros
 router
-    .get('/livros', livrosController.listarLivros)//se chamarem um get para o /livros faça o livros.Controller.listarLivros
+    .get('/livros', livrosController.listarLivros)
     .post('/livros', livrosController.cadastrarLivro)
-    .put('/livros?:id', livrosController.atualizarLivro)
+    .put('/livros?:id', livrosController.atualizarLivro);
 
 export default router;
