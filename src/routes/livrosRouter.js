e679084ad8c566faa8e224bcc7501f4c8f521187
sabiss <sabrinabezsilva@gmail.com>
2023-07-19 19:51:56 -0300

    nova rota

--- src/routes/livrosRouter.js
+++ src/routes/livrosRouter.js
@@ -5,6 +5,7 @@ const router = express.Router();
 //se chamarem um get para o /livros faça o livros.Controller.listarLivros
 router
     .get('/livros', livrosController.listarLivros)
+    .get('/livros/:id', livrosController.listarLivroPorID)
     .post('/livros', livrosController.cadastrarLivro)
     .put('/livros/:id', livrosController.atualizarLivro);//os dois pontos indica que ali vai chegar um parâmetro do tipo id
 
